08e8fbde178f7ef1c530c6c7b62b70e4b74ace28
dipti-solanki <diptisolanki09@gmail.com>
2020-03-25 10:38:54 +0530

    web(code):updated program.cs

--- MovieManagement/Program.cs
+++ MovieManagement/Program.cs
@@ -8,32 +8,29 @@ namespace MovieManagement
         {
             Console.WriteLine("Welcome to our application. Hope you'll have a good experience.");
             int choice;
-            Console.WriteLine("1. See the Details of Movie");
-            Console.WriteLine("2. See the Details of Actor/Actress");
-            Console.WriteLine("3. Add any new Actor/Actress Detail");
-            Console.WriteLine("4. Add any new Movie Detail");
-            Console.WriteLine("5. Delete information of a actor");
-            Console.WriteLine("6. Delete information of a movie");
+            
+            Console.WriteLine("1. Add any new Actor/Actress Detail");
+            Console.WriteLine("2. Add any new Movie Detail");
+            Console.WriteLine("3. Delete information of a actor");
+            Console.WriteLine("4. Delete information of a movie");
             Console.WriteLine("Enter your choice");
             choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
             {
-                case 1:break;
-                case 2:break;
-                case 3:Actor actor = new Actor();
+                case 1:Actor actor = new Actor();
                     actor.Add();
                     break;
-                case 4:
-                    Movie movie = new Movie();
+                case 2:Movie movie = new Movie();
                     movie.Add();
                     break;
-                case 5:
-                    Actor actor1 = new Actor();
+                case 3: Actor actor1 = new Actor();
                     actor1.Delete();
                     break;
-                case 6: Movie movie1 = new Movie();
+                case 4: Movie movie1 = new Movie();
                     movie1.Delete();
                     break;
+                    
+                
                 default:Console.WriteLine("Please enter valid choice");
                     break;
             }
